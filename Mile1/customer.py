79a4ea5ffe4364456adc87f4d72cfa79fe81a6f6
Anastasia <savicanastasia16@gmail.com>
2026-09-18 17:09:16 -0400

    added docstrings
    Added docstrings to the Customer class and its methods for better documentation.

--- Mile1/customer.py
+++ Mile1/customer.py
@@ -2,16 +2,21 @@ from cart import ShoppingCart
 
 
 class Customer:
+    """Represents a custumer, and their shopping cart"""
     def __init__(self, customer_id: str, name: str):
+        """Creates a customer with an ID, name, and empty shopping cart"""
         self.cart = ShoppingCart()
         self.customer_id = customer_id
         self.name = name
 
     def get_id(self):
+        """returns the customer ID"""
         return self.customer_id
 
     def get_name(self):
+        """Returns the customer name"""
         return self.name
 
     def get_cart(self):
+        """Returns the customer's shopping cart"""
         return self.cart
