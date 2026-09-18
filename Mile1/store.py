deee02b55f8d7399a74f0b1631d5e4848ef13ccf
Anastasia <savicanastasia16@gmail.com>
2026-09-18 16:06:51 -0400

    Refactor Store class

--- Mile1/store.py
+++ Mile1/store.py
@@ -1,30 +1,30 @@
 class Store:
     def __init__(self):
-        self.inventory = []
-        self.shoppers = []
+        self.products = []
+        self.customers = []
 
     def add_product(self, product):
-        for i in range(len(self.inventory)):
-                if (self.inventory[i].get_id() == product.get_id()):
+        for i in range(len(self.products)):
+                if (self.products[i].get_id() == product.get_id()):
                     return False
-        self.inventory.append(product)
+        self.products.append(product)
         return True
     
     def find_product(self, product_id):
-        for i in range(len(self.inventory)):
-            if (self.inventory[i].get_id() == product_id):
-                return self.inventory[i]
+        for i in range(len(self.products)):
+            if (self.products[i].get_id() == product_id):
+                return self.products[i]
         return None
                  
     def add_customer(self, customer):
-        for i in range(len(self.shoppers)):
-                 if (self.shoppers[i].get_id() == customer.get_id()):
+        for i in range(len(self.customers)):
+                 if (self.customers[i].get_id() == customer.get_id()):
                     return False
-        self.inventory.append(customer)
+        self.customers.append(customer)
         return True
 
     def find_customer(self, customer_id):
-        for i in range(len(self.shoppers)):
-            if (self.shoppers[i].get_id() == customer_id):
-                return self.shoppers[i]
+        for i in range(len(self.customers)):
+            if (self.customers[i].get_id() == customer_id):
+                return self.customers[i]
         return None
