eb574c50f44632a3c26f724f203117db3db4b893
Anastasia <savicanastasia16@gmail.com>
2026-09-18 16:16:13 -0400

    Refactor ShoppingCart

--- Mile1/cart.py
+++ Mile1/cart.py
@@ -1,28 +1,28 @@
 class ShoppingCart:
     def __init__(self):
-        self.cart = []
+        self.items = []
 
-    def add_product(self, product):
-        self.cart.append(product)
+    def add_product(self, product: Product):
+        self.items.append(product)
 
-    def remove_product(self, product_id):
-        for i in range(len(self.cart)):
-            if self.cart[i].get_id() == product_id:
-                self.cart.pop(i)
+    def remove_product(self, product_id: str):
+        for i in range(len(self.items)):
+            if self.items[i].get_id() == product_id:
+                self.items.pop(i)
                 return True
         return False
 
     def get_items(self):
-        return self.cart
+        return self.items
 
     def calculate_total(self):
-        sum = 0.0
-        for i in range(len(self.cart)):
-            sum += self.cart[i].get_price()
-        return sum
+        total = 0.0
+        for i in range(len(self.items)):
+            total += self.items[i].get_price()
+        return total
 
     def is_empty(self):
-        if self.cart == []:
+        if self.items == []:
             return True
         else:
             return False
