b9563bf8081e5d6938d029669c6728ee0cf433e6
Anastasia <savicanastasia16@gmail.com>
2026-09-18 10:04:44 -0400

    Update product.py

--- Mile1/product.py
+++ Mile1/product.py
@@ -3,3 +3,9 @@ class Product:
         self.product_id = product_id
         self.name = name
         self.price = price
+    def get_id(self):
+        return self.product_id
+    def get_name(self):
+        return self.name
+    def get_price(self):
+        return self.price
