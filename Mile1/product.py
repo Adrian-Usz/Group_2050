ae9d1651f9dba6a73637546a9bafbb0386e8b67e
Anastasia <savicanastasia16@gmail.com>
2026-09-18 16:27:54 -0400

    added docstrings

--- Mile1/product.py
+++ Mile1/product.py
@@ -1,13 +1,21 @@
 class Product:
+    """This represents a product sold by the store"""
     def __init__(self, product_id: str, name: str, price: float):
+        """This creates a product with an ID, listed as a string, a name (also a string), and price (float)"""
+        
         self.product_id = product_id
         self.name = name
         self.price = price
 
     
     def get_id(self):
+        """returns the product ID"""
         return self.product_id
+        
     def get_name(self):
+        """returns the product name"""
         return self.name
+        
     def get_price(self):
+        """returns the price of the product"""
         return self.price
